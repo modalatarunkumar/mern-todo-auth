5f6be1534985701409190ca8e2ff063020d6dc2d
modala tarun kumar <modalatarunkumar@gmail.com>
2025-12-01 13:15:04 +0530

    logout done

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -105,7 +105,9 @@ export const login = asyncHandler(async (req, res) => {
 export const logout = asyncHandler((_req, res) => {
     res.cookie("token", null, {
         expires: new Date(Date.now()),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
     })
     res.status(200).json({
         success: true,
