4708b5b7ddd44e0b6f3749d28fa550613d567873
modala tarun kumar <modalatarunkumar@gmail.com>
2025-12-01 13:08:55 +0530

    cors setting done

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -7,7 +7,9 @@ import config from "../config/index.js";
 
 export const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-    httpOnly: true
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
 }
 
 /******************************************************
